880267505bf82c6f399add0f4cbb8e1fd0904b22
Maxim <maximoweller@gmail.com>
2021-10-11 19:45:37 +0200

    Now command can only be used in a voice channel

--- commands/musicCommands/playnext.js
+++ commands/musicCommands/playnext.js
@@ -4,6 +4,8 @@ module.exports = {
     description: "Command to add the next song despite possible queued up songs",
     aliases: ['pn'],
     async execute(message, arguments, distube) {
+        const voiceChannel = message.member.voice.channel;
+        if (!voiceChannel) return message.reply('You have to be in a voice channel to play music!');
         if (distube.getQueue(message) === undefined) return message.reply(`There is nothing in the upcoming queue, please use the **${require(`./play`).name}**-command! :rolling_eyes:`);
         await distube.play(message, arguments.join(' '), {unshift: true});
         return message.channel.send(`Next song is going to be **${(distube.getQueue(message).songs)[1].name}** :nerd:`);
